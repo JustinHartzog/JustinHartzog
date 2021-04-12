2248e581022740e08dfccae2eac56e9fe2b043f4
JustinHartzog <42506583+JustinHartzog@users.noreply.github.com>
2021-04-11 23:01:03 -0600

    added crowdfund

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -11,6 +11,8 @@ function contentAnimation() {
 }
 */
 
+/*
+
 var tl = gsap.timeline({defaults:{duration: .9, ease: Back.easeOut.config(1), opacity: 0}})
         var tl2 = gsap.timeline({defaults:{duration: 1.5, delay:1}})
         var tl3 = gsap.timeline({defaults:{duration: 1.5}})
@@ -18,3 +20,4 @@ var tl = gsap.timeline({defaults:{duration: .9, ease: Back.easeOut.config(1), op
         tl.from(".grid-container", {delay: 1, scale: .2, transformOrigin: 'top'}, "=.2")
           .from(".lead", {scaleY:0, transformOrigin: 'top'})
           .from(".grid-x", {scale: .2, transformOrigin: 'center'}, "-=.7")
+*/
